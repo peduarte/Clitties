5a5894d9d6cb6576d538cc184e7fc6b6bf7e2ff7
Pedro Duarte <contact@peduarte.com>
2014-01-31 21:50:24 +0000

    Bunch of improvements lol

--- scripts.js
+++ scripts.js
@@ -8,40 +8,72 @@ $(function () {
   var $body = $('body');
   var $clit = $('.clit');
   var $labia = $('.labia');
+  var pfx = ['webkit', 'moz', 'MS', 'o', ''];
+  var anim = document.getElementById('main-drop');
+  var isTouch = (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0));
 
-  $body.on('mousewheel', function(event) {
 
-    if (event.deltaY > movementThreshold) { event.deltaY = movementThreshold; }
-    else if (event.deltaY < -movementThreshold) { event.deltaY = -movementThreshold; }
+  if (isTouch) {
 
+    $(document).on('touchmove',function(event){
+      event.preventDefault();
+
+      applyTransforms(movementThreshold, i);
+
+      i++;
+
+    });
+
+  }
+
+  else {
+
+    $body.on('mousewheel', function(event) {
+      event.preventDefault();
+
+      if (event.deltaY > movementThreshold) { event.deltaY = movementThreshold; }
+      else if (event.deltaY < -movementThreshold) { event.deltaY = -movementThreshold; }
+
+      applyTransforms(event.deltaY, i);
+
+      i++;
+
+    });
+
+  }
+
+
+  function applyTransforms(position, index) {
     $clit.css({
-      '-webkit-transform': 'translate3d(' + getRandomInt(-event.deltaY/4, event.deltaY/4) + 'px, ' + getRandomInt(-event.deltaY, event.deltaY) + 'px, 0)'
+      '-webkit-transform': 'translate3d(' + getRandomInt(-position/4, position/4) + 'px, ' + getRandomInt(-position, position) + 'px, 0)',
+      '-moz-transform': 'translate3d(' + getRandomInt(-position/4, position/4) + 'px, ' + getRandomInt(-position, position) + 'px, 0)',
+      'transform': 'translate3d(' + getRandomInt(-position/4, position/4) + 'px, ' + getRandomInt(-position, position) + 'px, 0)'
     });
 
     $labia.css({
-      '-webkit-transform': 'translate3d(' + getRandomInt(-event.deltaY/8, event.deltaY/8) + 'px, ' + getRandomInt(-event.deltaY/2, event.deltaY/2) + 'px, 0)'
+      '-webkit-transform': 'translate3d(' + getRandomInt(-position/8, position/8) + 'px, ' + getRandomInt(-position/2, position/2) + 'px, 0)',
+      '-moz-transform': 'translate3d(' + getRandomInt(-position/8, position/8) + 'px, ' + getRandomInt(-position/2, position/2) + 'px, 0)',
+      'transform': 'translate3d(' + getRandomInt(-position/8, position/8) + 'px, ' + getRandomInt(-position/2, position/2) + 'px, 0)'
     });
 
-    if (i > orgasm1 && !climaxAchieved) {
+    if (index > orgasm1 && !climaxAchieved) {
       climaxAchieved = true;
       $body.addClass('orgasm--1');
     }
+  }
 
-
-    i++;
-
-  });
 
   function climax () {
     $body.addClass('climax');
-    $body.unmousewheel();
+    if (isTouch) {
+      $body.unmousewheel();
+    }
   }
 
   function getRandomInt (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  var pfx = ['webkit', 'moz', 'MS', 'o', ''];
   function PrefixedEvent(element, type, callback) {
     for (var p = 0; p < pfx.length; p++) {
       if (!pfx[p]) type = type.toLowerCase();
@@ -49,7 +81,6 @@ $(function () {
     }
   }
 
-  var anim = document.getElementById('main-drop');
   PrefixedEvent(anim, 'AnimationEnd', function () {
     climax();
   });
